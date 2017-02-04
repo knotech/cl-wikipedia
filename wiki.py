b9538c110b5eec885b2dd86533e89f07584f709e
knotech <gabe.schroder@gmail.com>
2017-02-04 09:44:26 -0600

    Mostly broke wikipedia into a module, still need to deal with disambiguation pages.

--- wiki.py
+++ wiki.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
 
-from bs4 import BeautifulSoup as page
-from curses import wrapper
 import curses
 import requests
 import sys
 import time
+from bs4 import BeautifulSoup as page
+from curses import wrapper
+from sources.wikipedia import *
+from utils.scrape import *
+
 
 stdscr = curses.initscr()
 curses.noecho()
@@ -19,73 +22,9 @@ def parse_args_to_query():
         print('Usage: wiki <your search as separate strings>')
         print('\n')
 
-    elif len(sys.argv) > 1:
-        query = sys.argv[1].capitalize()
-
-    if len(sys.argv) > 2:
-        for arg in sys.argv[2:]:
-            if arg != ('the' or 'an' or 'and' or 'or' or 'in' or 'on'):
-                print(arg)
-                arg = arg.capitalize()
-                print(arg)
-                query += '_'
-                query += arg
-
+    else:
+        query = build_wiki_query(sys.argv)
     return query
-
-
-def get_page(url):
-    res = requests.get(url)
-    return res.text
-
-
-def map_DOM(webpage):
-    webpage = page(webpage, 'html.parser')
-    return webpage
-
-
-def is_valid_wiki_query(webpage):
-    check = webpage.find(id='noarticletext')
-    if check == None:
-        return True
-    else:
-        return False
-
-
-def isolate_content(webpage):
-    content_container = webpage.find(id='mw-content-text')
-    return content_container
-
-
-def get_paragraphs(content):
-    paragraphs = content.find_all('p')
-    return paragraphs
-
-
-def clean_paragraphs(paragraphs):
-    clean_paragraphs = []
-    for p in paragraphs:
-        p = p.get_text()
-        if p != '':
-            clean_paragraphs.append(p)
-            clean_paragraphs.append('NEWLINE')
-        else:
-            pass
-    return clean_paragraphs
-
-
-def paragraph_list():
-    query_string = parse_args_to_query()
-    raw_page = get_page('https://en.wikipedia.org/wiki/' + query_string)
-    mapped_page = map_DOM(raw_page)
-    if is_valid_wiki_query(mapped_page):
-        content = isolate_content(mapped_page)
-        target_text = get_paragraphs(content)
-        display_text = clean_paragraphs(target_text)
-        return display_text
-    else:
-        print(query_string + 'Try different capitalization')
-        sys.exit(1)
 
 
 def line_wrap(text, max_chars):
@@ -120,7 +59,7 @@ def main(stdscr):
     max_rows = 40
 
     # Convert list of paragraphs to lines
-    paragraphs = paragraph_list()
+    paragraphs = format_content()
     lines = line_wrap(' '.join(paragraphs), max_cols)
 
     top_line = 0
